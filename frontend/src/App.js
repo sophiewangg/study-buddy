42079f5c511628eb8e34533231783eb1eb1355e7
sophiewangg <55284221+sophiewangg@users.noreply.github.com>
2022-12-31 13:55:56 -0500

    added frontend ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Header from './components/Header';
-import Dashboard from './pages/dashboard';
-import Login from './pages/login';
-import Register from './pages/register';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
